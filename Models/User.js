08b6b2f9614104cc95805a5fa2efcdd880cf8b2e
AD <archied5150@yahoo.com>
2022-06-02 13:27:41 -0500

    user schema

--- Models/User.js
+++ Models/User.js
@@ -1,7 +1,40 @@
 const router = require('express').Router();
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({});
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: 'Username is required',
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: 'Email is required',
+      trim: true,
+      match: [/.+\@.+\..+/, 'please fill an valid email address'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [this],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
 
 const User = model('User', UserSchema);
 
